09895d724f1edaee419655c9a5413c3b5dfafc52
Salam Waddah <salamwaddah@gmail.com>
2020-05-12 22:58:03 +0400

    ITE6102-Week4-HelloWorld

---
+++ ITE6102/Week4HelloWorld/main.cpp
@@ -0,0 +1,43 @@
+/* ****************************************************************
+ * @author: <your name here>
+ * @app name: <your application name here>
+ * @app desc: <describe the purpose of your application here>
+ * @history:
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ *  - <yyyy/mm/dd> | <name>
+ *      -- <description>
+ *      -- <description>
+ *      -- <description>
+ * ****************************************************************/
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "pause.h"
+
+using namespace std;
+
+//////////////////////////////////////////////////////////////////
+//                               NOTE
+// This is your program entry point. Your main logic is placed
+// inside this function. You may add your functions before this
+// "main()", or after this "main()" provided you added reference
+// before this "main()" function.
+//////////////////////////////////////////////////////////////////
+
+int main() {
+    // ************************** TO DO **************************
+    // Place your code logic after this comment line
+    // ***********************************************************
+
+    cout << "Hello World!";
+
+    // ********************** DO NOT CHANGE **********************
+    // Print a new line and ask user for any key before exiting
+    // ***********************************************************
+    _pause();
+    return EXIT_SUCCESS;
+}
